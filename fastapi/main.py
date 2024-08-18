75533e7154e1eccbcdd9e5b481bdc2d4d51d3a5a
changwoo <124979889+okchangwoo@users.noreply.github.com>
2024-08-18 16:15:36 +0900

    add: face-recognition

--- fastapi/main.py
+++ fastapi/main.py
@@ -7,6 +7,17 @@ import torch
 import numpy as np
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 
+import face_recognition
+from typing import List
+from models.fr_modules import face_distance, compare_faces_with_similarity
+
+
+# 이미지 업로드 함수
+def load_image_from_upload(upload_file: UploadFile):
+    image = Image.open(io.BytesIO(upload_file.file.read()))
+    return np.array(image)
+
+
 app = FastAPI()
 
 origins = [
@@ -30,42 +41,75 @@ try:
 except Exception as e:
     print(f"Error loading model: {str(e)}")
 
+
 @app.post("/predict")
 async def predict(file: UploadFile):
     try:
         contents = await file.read()
-        
+
         # 이미지를 PIL 포맷으로 변환
         image = Image.open(io.BytesIO(contents))
 
         # RGB로 변환
-        image = image.convert('RGB')
-        
+        image = image.convert("RGB")
+
         # NumPy 배열로 변환
         image_np = np.array(image)
-        
+
         # 이미지 전처리
         inputs = processor(images=image_np, return_tensors="pt")
-        
+
         # 모델을 통해 예측 수행
         with torch.no_grad():
             outputs = model(**inputs)
-        
+
         # logits에서 예측 클래스와 확률 계산
         logits = outputs.logits
         probabilities = torch.nn.functional.softmax(logits, dim=-1)
-        
+
         # 결과를 저장
         results = []
         for i, (logit, probability) in enumerate(zip(logits[0], probabilities[0])):
             class_name = model.config.id2label[i]
             rounded_probability = round(probability.item(), 2)
             results.append({"class": class_name, "probability": rounded_probability})
-        
+
         # 확률에 따라 내림차순으로 정렬
         results.sort(key=lambda x: x["probability"], reverse=True)
-        
+
         return JSONResponse(content={"predictions": results[:3]})
     except Exception as e:
         print(f"Error in predict function: {str(e)}")
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+# compare 추가
+@app.post("/compare-faces/")
+async def compare_faces(known_image_file: UploadFile, unknown_image_file: UploadFile):
+    # Load the images from the uploaded files
+    known_image = load_image_from_upload(known_image_file)
+    unknown_image = load_image_from_upload(unknown_image_file)
+
+    # Encode the faces
+    try:
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    except IndexError:
+        raise HTTPException(
+            status_code=400, detail="No face found in one of the images."
+        )
+
+    # Compare faces
+    tolerance = 0.6
+    results = face_recognition.compare_faces(
+        [known_encoding], unknown_encoding, tolerance=tolerance
+    )
+    # 얼굴 비교 및 유사도 계산
+    similarity = compare_faces_with_similarity([known_encoding], unknown_encoding)
+
+    # Convert numpy.bool_ to standard bool (fastapi가 numpy.bool_를 fastapi가 처리 못함)
+    return {
+        "match": bool(results[0]),
+        "tolerance": tolerance,
+        "similarity": similarity[0][1],
+    }
