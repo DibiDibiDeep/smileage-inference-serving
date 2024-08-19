bdb02c9c2a83d3b8e2d9b60868128f41c68e057b
okchang95 <okchang95@gmail.com>
2024-08-19 16:04:28 +0900

    add: HTTPException

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from PIL import Image
